071bad5ce371c00f53f540b6e52dc82ac75ec8f6
k-wiese <letmeheares@gmail.com>
2021-03-21 12:19:59 +0100

    Update mail_handler.php

--- mail_handler.php
+++ mail_handler.php
@@ -10,10 +10,12 @@
 		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
 		$headers="From: ".$email;
 
-		if(mail($to, $subject, $message, $headers)){
+		if(mail($to, $subject, $message, $headers))
+		{
 			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
 		}
-		else{
+		else
+		{
 			echo "Something went wrong!";
 		}
 	}
